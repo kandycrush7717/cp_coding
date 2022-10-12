c4a4b7905f1738af9d1e33eada0d72af93e94cee
venkatapraveen kandimalla <venkatapraveen.kandimalla@gmail.com>
2022-10-12 18:22:41 -0400

    added codechef Enrmous Input Test

--- codechef/enrmous_input_test.py
+++ codechef/enrmous_input_test.py
@@ -2,6 +2,31 @@
     * Author : VenkataPraveen Kandimalla
     * Date : 2022-10-12
     * Time : 12:56 a.m.
-    * Question :
+    * Question :(https://www.codechef.com/LP0TO101/problems/INTEST)
+        The purpose of this problem is to verify whether the method you are using to read input data is sufficiently
+        fast to handle problems branded with the enormous Input/Output warning. You are expected to be able to process
+        at least 2.5MB of input data per second at runtime.
     * Example :
+        Sample 1:
+            Input
+            7 3
+            1
+            51
+            966369
+            7
+            9
+            999996
+            11
+            Output
+            4
+
 '''
+import sys
+if __name__=='__main__':
+    n,k=[int(x) for x in sys.stdin.readline().rstrip('\n').split(' ')]
+    count=0
+    for _ in range(n):
+        t=int(sys.stdin.readline().rstrip('\n'))
+        if t%k==0:
+            count+=1
+    sys.stdout.write(str(count)+'\n')
