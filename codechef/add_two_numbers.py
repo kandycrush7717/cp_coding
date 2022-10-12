775235de63992204b6235c488105e3aecdb3e17c
venkatapraveen kandimalla <venkatapraveen.kandimalla@gmail.com>
2022-10-12 18:24:19 -0400

    added codechef Add Two Numbers

--- codechef/add_two_numbers.py
+++ codechef/add_two_numbers.py
@@ -2,6 +2,22 @@
     * Author : VenkataPraveen Kandimalla
     * Date : 2022-10-12
     * Time : 1:17 a.m.
-    * Question :
+    * Question :(https://www.codechef.com/LP0TO101/problems/FLOW001)
+        The task is very simple: given two integers A and B, write a program to add these two numbers and output it.
     * Example :
+        Input:
+        3
+        1 2
+        100 200
+        10 40
+        Output:
+        3
+        300
+        50
 '''
+import sys
+if __name__=='__main__':
+    t=int(sys.stdin.readline().rstrip('\n'))
+    for _ in range(t):
+        a,b=[int(x) for x in sys.stdin.readline().rstrip('\n').split(' ')]
+        sys.stdout.write(str(a+b)+'\n')
