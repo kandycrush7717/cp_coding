02526bfba74c8d3ef12c38ae34876eae795e2130
venkatapraveen kandimalla <venkatapraveen.kandimalla@gmail.com>
2022-10-12 18:19:05 -0400

    added codechef Sum Of Digits

--- codechef/sum_of_digits.py
+++ codechef/sum_of_digits.py
@@ -2,6 +2,25 @@
     * Author : VenkataPraveen Kandimalla
     * Date : 2022-10-12
     * Time : 1:07 a.m.
-    * Question :
+    * Question :(https://www.codechef.com/LP0TO101/problems/FLOW006)
+        You're given an integer N. Write a program to calculate the sum of all the digits of N.
     * Example :
+        Input
+        3
+        12345
+        31203
+        2123
+        Output
+        15
+        9
+        8
 '''
+import sys
+if __name__=='__main__':
+    t=int(sys.stdin.readline().rstrip('\n'))
+    for _ in range(t):
+        n=sys.stdin.readline().rstrip('\n')
+        digits_sum=0
+        for i in n:
+            digits_sum+=(int(i))
+        sys.stdout.write(str(digits_sum)+'\n')
