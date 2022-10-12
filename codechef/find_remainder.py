772780642e0379cff377b15f06402e222ba51c49
venkatapraveen kandimalla <venkatapraveen.kandimalla@gmail.com>
2022-10-12 18:22:12 -0400

    added codechef Find Remainder

--- codechef/find_remainder.py
+++ codechef/find_remainder.py
@@ -2,6 +2,22 @@
     * Author : VenkataPraveen Kandimalla
     * Date : 2022-10-12
     * Time : 1:03 a.m.
-    * Question :
+    * Question :(https://www.codechef.com/LP0TO101/problems/FLOW002)
+        Write a program to find the remainder when an integer A is divided by an integer B.
     * Example :
+        Input:
+        3
+        1 2
+        100 200
+        40 15
+        Output:
+        1
+        100
+        10
 '''
+import sys
+if __name__=='__main__':
+    t=int(sys.stdin.readline().rstrip('\n'))
+    for _ in range(t):
+        a,b=[int(x) for x in sys.stdin.readline().rstrip('\n').split(' ')]
+        sys.stdout.write(str(a%b)+'\n')
