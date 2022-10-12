3211bee06d9491115bcbc80e9bdbafb1a7b5bd4d
venkatapraveen kandimalla <venkatapraveen.kandimalla@gmail.com>
2022-10-12 18:21:18 -0400

    added codechef Id and Ship

--- codechef/id_and_ship.py
+++ codechef/id_and_ship.py
@@ -2,6 +2,28 @@
     * Author : VenkataPraveen Kandimalla
     * Date : 2022-10-12
     * Time : 5:53 p.m.
-    * Question :
+    * Question :(https://www.codechef.com/LP0TO101/problems/FLOW010)
+        Write a program that takes in a letterclass ID of a ship and display the equivalent string class description
+        of the given ID. Use the table below.
     * Example :
+        Input
+        3
+        B
+        c
+        D
+
+
+        Output
+        BattleShip
+        Cruiser
+        Destroyer
 '''
+import sys
+if __name__=='__main__':
+    t=int(sys.stdin.readline().rstrip('\n'))
+    id_dict={'b':'BattleShip','c':'Cruiser','d':'Destroyer','f':'Frigate'}
+    for _ in range(t):
+        id=sys.stdin.readline().rstrip('\n')
+        id=id.lower()
+        if id in id_dict:
+            sys.stdout.write(id_dict[id]+'\n')
