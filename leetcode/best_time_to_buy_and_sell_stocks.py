d2276dfbf6d19cb54456e881c4557d4b444bfe47
venkatapraveen kandimalla <venkatapraveen.kandimalla@gmail.com>
2022-11-08 11:58:19 -0500

    added leetcode Best Time to Buy and Sell Stocks

---
+++ leetcode/best_time_to_buy_and_sell_stocks.py
@@ -0,0 +1,29 @@
+'''
+    * Author : VenkataPraveen Kandimalla
+    * Date : 2022-11-08
+    * Time : 11:32 a.m.
+    * Question :(https://leetcode.com/problems/best-time-to-buy-and-sell-stock/)
+        You are given an array prices where prices[i] is the price of a given stock on the ith day.
+        You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in
+        the future to sell that stock.
+        Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+    * Example :
+        Input: prices = [7,1,5,3,6,4]
+        Output: 5
+        Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+        Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+'''
+import sys
+if __name__=='__main__':
+    t=int(sys.stdin.readline().rstrip('\n'))
+    for _ in range(t):
+        prices=[int(x) for x in sys.stdin.readline().rstrip('\n').split()]
+        profit = 0
+        n = len(prices)
+        buy = prices[0]
+        for i in range(1, n):
+            if prices[i] > buy:
+                profit = max(profit, prices[i] - buy)
+            else:
+                buy = prices[i]
+        sys.stdout.write(str(profit)+'\n')
